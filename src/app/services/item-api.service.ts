84b9f3c78ce714aa7efb96535debeabaec3774b0
Marcio Junior <marciofmjr@hotmail.com>
2022-03-15 23:06:14 -0300

    add tests for item api service

--- src/app/services/item-api.service.ts
+++ src/app/services/item-api.service.ts
@@ -6,39 +6,44 @@ import { ApiService } from './api.service';
 import { Item } from '../models/item.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemApiService {
-
   private items$ = new Subject<Item[]>();
   private items: Item[] = [];
   private path = 'items';
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
   getItems(): Observable<Item[]> {
     return this.items$.asObservable();
   }
 
   delete(id: string): Observable<Item> {
-    return this.apiService.delete<Item>(this.path, id).pipe(tap(deletedItem => {
-      this.items = this.items.filter((item) => item.id !== deletedItem.id);
-      this.notifyChange();
-    }));
+    return this.apiService.delete<Item>(this.path, id).pipe(
+      tap((deletedItem) => {
+        this.items = this.items.filter((item) => item.id !== deletedItem.id);
+        this.notifyChange();
+      })
+    );
   }
 
   deleteAll(): Observable<Item[]> {
-    return this.apiService.delete<Item[]>(this.path).pipe(tap(items => {
-      this.items = [];
-      this.notifyChange();
-    }));
+    return this.apiService.delete<Item[]>(this.path).pipe(
+      tap((items) => {
+        this.items = [];
+        this.notifyChange();
+      })
+    );
   }
 
   create(item: Item): Observable<Item> {
-    return this.apiService.save(item, this.path).pipe(tap(createdItem => {
-      this.items.unshift({...createdItem, visible: true});
-      this.notifyChange();
-    }));
+    return this.apiService.save(item, this.path).pipe(
+      tap((createdItem) => {
+        this.items.unshift({ ...createdItem, visible: true });
+        this.notifyChange();
+      })
+    );
   }
 
   updateDone(id: string, done: boolean): Observable<Item> {
@@ -50,7 +55,7 @@ export class ItemApiService {
   }
 
   search(searchText: string): void {
-    this.items = this.items.map(item => {
+    this.items = this.items.map((item) => {
       item.visible = item.title.includes(searchText);
       return item;
     });
@@ -58,8 +63,8 @@ export class ItemApiService {
   }
 
   getAll(): void {
-    this.apiService.get<Item[]>(this.path).subscribe(items => {
-      this.items = items.map(item => {
+    this.apiService.get<Item[]>(this.path).subscribe((items) => {
+      this.items = items.map((item) => {
         item.visible = true;
         return item;
       });
@@ -70,5 +75,4 @@ export class ItemApiService {
   notifyChange(): void {
     this.items$.next(this.items);
   }
-
 }
