96965c43226b91a5e8a5038725e7fc67cfc502ce
Marcio Junior <marciofmjr@hotmail.com>
2022-03-19 12:14:45 -0300

    crete playground page

---
+++ src/app/pages/playground/playground.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playground',
+  templateUrl: './playground.page.html',
+  styleUrls: ['./playground.page.sass'],
+})
+export class PlaygroundPage implements OnInit {
+  text = 'au';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
